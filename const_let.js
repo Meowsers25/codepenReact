d762d140966ea96f55800ee76f5a37e1a1fa6331
Chris <c.lamothe25@gmail.com>
2017-12-15 21:21:39 -0500

    const and let

---
+++ const_let.js
@@ -0,0 +1,18 @@
+var name = "Chris";
+console.log(name);
+
+name = "Katie";
+console.log(name);
+
+let dog = "Luna";
+console.log(dog);
+
+dog = "Beatrice";
+console.log(dog);
+
+const computer = "Apple";
+console.log(computer);
+
+//to show how const works
+computer = "Microsoft";
+console.log(computer);
